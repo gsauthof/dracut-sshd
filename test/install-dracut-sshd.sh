54a56d4042c243bd73c1b26da6a971555ddb0bad
Georg Sauthoff <mail@gms.tf>
2024-11-30 18:19:53 +0100

    enable networkd in guest for a smoother test run

--- test/install-dracut-sshd.sh
+++ test/install-dracut-sshd.sh
@@ -5,16 +5,14 @@ set -eux
 mydir=$(dirname -- "${BASH_SOURCE[0]}")
 . "$mydir"/conf.sh
 
+
 dracut_dir="$mydir"/..
-
-
-
 guest=$(get_addr "$tag")
 
 pushd "$dracut_dir"
 $scp  example/20-wired.network  root@"$guest":/etc/systemd/network/20-wired.network
 $scp  example/90-networkd.conf  root@"$guest":/etc/dracut.conf.d/90-networkd.conf
-$scp  -r 46sshd  root@"$guest":/usr/lib/dracut/modules.d/
+$scp  -r 46sshd                 root@"$guest":/usr/lib/dracut/modules.d/
 
 
 $ssh root@"$guest" <<EOF
@@ -22,5 +20,11 @@ set -eux
 # make sure dhcp client gets same address in early/late boot
 echo -e '\n[DHCPV4]\nClientIdentifier=mac' >> /etc/systemd/network/20-wired.network
 dnf -y install dracut-network
+
+# not strictly necessary, but keeps the IP-address of the guest stable
+systemctl disable NetworkManager
+systemctl mask NetworkManager
+systemctl enable systemd-networkd
+
 dracut -f -v
 EOF
