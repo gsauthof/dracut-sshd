0f80940ab2d73a559a2e75a110e620126d26dc27
Georg Sauthoff <mail@gms.tf>
2024-12-07 20:13:12 +0100

    support rawhide cloud images

--- test/get-fedora.sh
+++ test/get-fedora.sh
@@ -8,24 +8,31 @@ if [ $# -gt 0 ]; then
     release=$1
 fi
 
-latest=$(curl -sSf https://kojipkgs.fedoraproject.org/compose/cloud/ \
-    | awk -F'"' ' /latest-Fedora-Cloud-'"$release"'/ {
-        s=$(NF-1);
-        sub("/", "", s); print s;
-    }' \
-    | sort -V \
-    | tail -n 1 \
-    | grep '^latest[a-zA-Z0-9-]\+$')
+
+if [ "$release" = rawhide ]; then
+    cid=$(curl -sSf https://kojipkgs.fedoraproject.org/compose/rawhide/latest-Fedora-Rawhide/COMPOSE_ID \
+        | grep '^Fedora-Rawhide-[0-9a-z.]\+$')
+    version=${cid#Fedora-Rawhide-}
+    img_url=https://kojipkgs.fedoraproject.org/compose/rawhide/latest-Fedora-Rawhide/compose/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-Rawhide-"$version".x86_64.qcow2
+    img=Fedora-Cloud-Base-Generic-Rawhide-"$version".x86_64.qcow2
+else
+    latest=$(curl -sSf https://kojipkgs.fedoraproject.org/compose/cloud/ \
+	| awk -F'"' ' /latest-Fedora-Cloud-'"$release"'/ {
+	    s=$(NF-1);
+	    sub("/", "", s); print s;
+	}' \
+	| sort -V \
+	| tail -n 1 \
+	| grep '^latest[a-zA-Z0-9-]\+$')
 
 
-cid=$(curl -sSf https://kojipkgs.fedoraproject.org/compose/cloud/"$latest"/COMPOSE_ID \
-      | grep '^Fedora-Cloud[0-9.-]\+$' )
+    cid=$(curl -sSf https://kojipkgs.fedoraproject.org/compose/cloud/"$latest"/COMPOSE_ID \
+	  | grep '^Fedora-Cloud[0-9.-]\+$' )
+    version=${cid#Fedora-Cloud-}
+    img_url=https://kojipkgs.fedoraproject.org/compose/cloud/"$latest"/compose/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-"$version".x86_64.qcow2
+    img=Fedora-Cloud-Base-Generic-"$version".x86_64.qcow2
+fi
 
-version=${cid#Fedora-Cloud-}
-
-
-img_url=https://kojipkgs.fedoraproject.org/compose/cloud/"$latest"/compose/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-"$version".x86_64.qcow2
-img=Fedora-Cloud-Base-Generic-"$version".x86_64.qcow2
 
 if curl -sSf -o "$img" "$img_url" ; then
     # i.e. Fedora 41 scheme
