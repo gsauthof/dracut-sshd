1de7c699f5739f7aac36ae846ffe54a36e8be372
James Shubin <james@shubin.ca>
2025-12-02 19:45:58 -0500

    network: Allow specifying the config outside of networkd folder
    This is useful if you don't want the host to see your normal networkd
    config because it might conflict with what you've got going on there
    already.

--- 46sshd/module-setup.sh
+++ 46sshd/module-setup.sh
@@ -54,6 +54,11 @@ install() {
     /usr/bin/install -m 600 "$authorized_keys" \
             "$initdir/root/.ssh/authorized_keys"
 
+    if [ -e /etc/dracut-sshd/wired.network ]; then
+        /usr/bin/install -m 600 "/etc/dracut-sshd/wired.network" \
+            "$initdir/etc/systemd/network/wired.network"
+    fi
+
     inst_binary /usr/sbin/sshd
     inst_multiple -o /etc/sysconfig/sshd /etc/sysconfig/ssh \
             /etc/sysconfig/dracut-sshd
